568a3380118d22e991792a11b4de520baa035798
AndrewSukhov <igrok-cs@yandex.ru>
2020-08-14 12:36:08 +0300

    Создал таблицу "Категории"

---
+++ database/migrations/2020_08_14_083843_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('parent_id');
+            $table->integer('external_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('category');
+    }
+}
