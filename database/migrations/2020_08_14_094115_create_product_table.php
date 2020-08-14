f2f53ecb3c463575bb6be10e75ac9d22fd575fa3
AndrewSukhov <igrok-cs@yandex.ru>
2020-08-14 13:19:16 +0300

    Создал таблицу "Продукты"

---
+++ database/migrations/2020_08_14_094115_create_product_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->dateTime('creation_at');
+            $table->double('price', 10, 2);
+            $table->integer('quantity');
+            $table->integer('category_id');
+            $table->integer('external_id');
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product');
+    }
+}
